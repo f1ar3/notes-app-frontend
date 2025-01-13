a1e0978fcc23ec145e5d1e77df127282eb743c4c
f1ar3 <goshaapolonoff@yandex.ru>
2025-01-14 01:21:22 +0300

    feat: создать разметку, стилизацию и базовый функционал для Ruler

--- app/notes/[noteId]/page.tsx
+++ app/notes/[noteId]/page.tsx
@@ -1,15 +1,13 @@
 import {Editor} from "@/app/components/editor/Editor";
 import Toolbar from "@/app/components/toolbar/Toolbar";
-
-interface NoteIdPageProps {
-    params: Promise<{ noteId: string }>
-}
+import Ruler from "@/app/components/forms/Ruler";
 
 const NoteIdPage = async () => {
 
     return (
         <div className='min-h-screen bg-[#FAFBFD]'>
             <Toolbar />
+            <Ruler />
 
             <Editor />
         </div>

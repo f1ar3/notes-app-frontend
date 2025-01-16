592f5f19d57d5344ce6656b3d2ceda2439488a2c
f1ar3 <goshaapolonoff@yandex.ru>
2025-01-16 22:16:53 +0300

    feat: сделать верстку и некоторый функционал элементу, отвечающему за поиск

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import Search from "@/app/components/header/Search";
 
 const Header = () => {
     return (
-        <div></div>
+        <nav className='flex items-center justify-between h-full w-full'>
+            <div className='flex gap-3 items-center shrink-0 pr-6'>
+                <Link href='/'>
+                    <Image src='/logo.svg' alt='Logo' width={36} height={36} />
+                </Link>
+                <h3 className='text-xl'>G-Notes</h3>
+            </div>
+            <Search />
+            <div>
+
+            </div>
+        </nav>
     )
 }
 
--- app/components/header/Search.tsx
+++ app/components/header/Search.tsx
@@ -1,13 +1,71 @@
+'use client'
+
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {SearchIcon, XIcon} from "lucide-react";
+import {useRef, useState} from "react";
+import {useSearchParam} from "@/app/components/hooks/useSearchParam";
+
 const Search = () => {
+    const [search, setSearch] = useSearchParam();
+    const [value, setValue] = useState(search);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+    }
+
+    const handleClear = () => {
+        setValue('');
+        inputRef.current?.blur();
+        setSearch('');
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setSearch(value);
+        inputRef.current?.blur();
+    }
+
     return (
-        <div className='flex items-center space-x-4'>
-            <input
-                type="text"
-                placeholder='Search...'
-                className='hidden sm:block px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
-            />
+        <div className='flex-1 flex items-center justify-center'>
+            <form
+                onSubmit={handleSubmit}
+                className='relative max-w-[720px] w-full'
+            >
+                <Input
+                    value={value}
+                    onChange={handleChange}
+                    ref={inputRef}
+                    placeholder='Search...'
+                    className='md:text-base placeholder:text-neutral-800 px-14 w-full border-none
+                     focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px-1px_rgba(65,69,73,.15]
+                     bg-[#F0F4F8] rounded-full focus-visible:ring-0 focus:bg-white'
+                />
+                <Button
+                    type='submit'
+                    variant='ghost'
+                    size='icon'
+                    className='absolute left-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full'
+                >
+                    <SearchIcon />
+                </Button>
+                {value && (
+                    <Button
+                        onClick={handleClear}
+                        type='button'
+                        variant='ghost'
+                        size='icon'
+                        className='absolute right-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full'
+                    >
+                        <XIcon />
+                    </Button>
+                )}
+            </form>
         </div>
     )
 }
 
 export default Search
+
+
